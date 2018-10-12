9425b3e0983d3c9d3cb699f67dc13460f4f7819f
Dmitrijus <Ravus1989@mail.ru>
2018-10-12 14:54:29 +0300

    cacl

--- calculator-app.js
+++ calculator-app.js
@@ -10,22 +10,42 @@ document.getElementById('output').innerHTML = firstNumber
 function getResult() {
   if (operator == 'add') {
     document.getElementById("output").innerHTML = Number(firstNumber) + Number(secondNumber)
+    secondNumber = document.getElementById("output").innerHTML;
+    firstNumber = 0;
   }else if (operator == 'minus') {
     document.getElementById("output").innerHTML = Number(secondNumber) - Number(firstNumber)
+    secondNumber = document.getElementById("output").innerHTML;
+    firstNumber = 0;
   }else if (operator == 'divide') {
     document.getElementById("output").innerHTML = Number(secondNumber) / Number(firstNumber)
+    secondNumber = document.getElementById("output").innerHTML;
+    firstNumber = 0;
   }else if (operator == 'x') {
     document.getElementById("output").innerHTML = Number(secondNumber) * Number(firstNumber)
+    secondNumber = document.getElementById("output").innerHTML;
+    firstNumber = 0;
   }
 }
 
 function changeOperator(value) {
   if (!secondNumber) {
-  secondNumber = firstNumber
+  secondNumber = firstNumber;
   firstNumber = 0;
 } else
   getResult()
   operator = value;
+}
+function addNeg() {
+  teprNumber = Number(document.getElementById("output").innerHTML)
+  if (teprNumber > 0) {
+    secondNumber = "-" + teprNumber
+    firstNumber = 0;
+  }else if (teprNumber < 0) {
+    secondNumber = teprNumber - (teprNumber*2)
+    firstNumber = 0;
+  }else {
+    return
+  }
 }
 
 function init() {
